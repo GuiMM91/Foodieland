ed44b2f5a5dd0d850216d7623cd9ef29ee52d7cb
Guilherme Marques Mesquita <g.m.mesquita@hotmail.com>
2022-08-21 15:50:11 -0300

    feat: initial configurations

---
+++ Foodieland/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "./styles"
+
+export function Header(){ 
+    return(
+        <Container>
+            <img/>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <img/>
+            <img/>
+            <img/>
+        </Container>
+    )
+}
